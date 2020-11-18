56797430c0ffbf5dcd77938940b7cd0260530e76
Maharsh <mgheewala@hawk.iit.edu>
2020-11-18 17:06:53 -0600

    Added Home Page Basic Layout

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -5,18 +5,56 @@ import VideoCard from './VideoCard';
 function Home() {
     return (
         <div className="home">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+            <div className="videos">
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+                <VideoCard
+                    image="https://i.ytimg.com/an_webp/JmcqMDGj3DQ/mqdefault_6s.webp?du=3000&sqp=CJiT1v0F&rs=AOn4CLAqXmK0mI5p7q0wyxacKEaxOsAjxw"    
+                    title="CHOTI CHOTI CHEZO MAI BADI BADI KUSHI..."
+                    views="544K views"
+                    timestamp="12 hours ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnjKYUZPkZADrFGPyUzo7dyQEUkZouMWRrcUTYCTsw=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Mumbiker Nikhil"
+                />
+            </div>
         </div>
     )
 }
--- front-end/src/VideoCard.js
+++ front-end/src/VideoCard.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core'
+import React from 'react';
+import './VideoCard.css';
 
-function VideoCard({image, title, channel, views, timestamp}) {
+function VideoCard({image, title, views, timestamp, channel, channelImage}) {
     return (
         <div className="VideoCard">
-            <img src={image} alt="" />
-            <div className="video__info">
-                
+            <img src="" alt="" className="videoCard__thumbnail" />
+            <div className="videoCard__info">
+                <Avatar
+                    className="videoCard__avatar"
+                    alt={channel}
+                    src={channelImage}
+                />
+                <div className="videoCard__text">
+                    <h4>{title}</h4>
+                    <p>{channel}</p>
+                    <p>
+                        {views} . {timestamp}
+                    </p>
+                </div>
             </div>
         </div>
     )
 }
 
-export default VideoCard
+export default VideoCard;
